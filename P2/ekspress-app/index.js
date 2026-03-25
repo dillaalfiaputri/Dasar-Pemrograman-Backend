ff5d01cbbca28466df2686d9fc1dba6cab24823a
via <dillaalfiaputri@gmail.com>
2026-03-25 21:43:13 +0700

    Add files via upload

---
+++ P2/ekspress-app/index.js
@@ -0,0 +1,15 @@
+// import express
+import express from "express";
+
+// init express
+const app = express();
+
+// basic route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// listen on port 3000
+app.listen(3000, () => {
+  console.log('Server Running at http://localhost:3000');
+});
